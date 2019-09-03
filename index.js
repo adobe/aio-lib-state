36a9e765f8ca7930d58e4553c07b0d5dac05753d
Moritz Raho <raho@adobe.com>
2019-09-03 14:17:45 +0200

    unit test for init + moved index to init + wrap 401 and 403 from tvm

--- index.js
+++ index.js
@@ -9,57 +9,5 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-const { CosmosStateStore } = require('./lib/impl/CosmosStateStore')
-const { StateStoreError } = require('./lib/StateStoreError')
-const { StateStore } = require('./lib/StateStore')
-const TvmClient = require('@adobe/adobeio-cna-tvm-client')
 
-/**
- * Initializes and returns the key-value-store SDK.
- *
- * To use the SDK you must either provide your
- * [OpenWhisk credentials]{@link module:types~OpenWhiskCredentials} in
- * `config.ow` or your own
- * [Azure Cosmos credentials]{@link module:types~AzureCosmosMasterCredentials} in `config.cosmos`.
- *
- * OpenWhisk credentials can also be read from environment variables (`OW_NAMESPACE` or `__OW_NAMESPACE` and `OW_AUTH` or `__OW_AUTH`).
- *
- * @param {object} [config={}] used to init the sdk
- *
- * @param {module:types~OpenWhiskCredentials} [config.ow]
- * {@link module:types~OpenWhiskCredentials}. Set those if you want
- * to use ootb credentials to access the state management service. OpenWhisk
- * namespace and auth can also be passed through environment variables:
- * `__OW_NAMESPACE` and `__OW_AUTH`
- *
- * @param {module:types~AzureCosmosMasterCredentials|module:types~AzureCosmosPartitionResourceCredentials} [config.cosmos]
- * [Azure Cosmos resource credentials]{@link module:types~AzureCosmosPartitionResourceCredentials} or
- * [Azure Cosmos account credentials]{@link module:types~AzureCosmosMasterCredentials}
- *
- * @param {object} [config.tvm] tvm configuration, applies only when passing OpenWhisk credentials
- * @param {string} [config.tvm.apiUrl] alternative tvm api url.
- * @param {string} [config.tvm.cacheFile] alternative tvm cache file, set to `false` to disable caching of temporary credentials.
- * @returns {Promise<StateStore>} A StateStore instance
- * @throws {StateStoreError}
- */
-async function init (config = {}) {
-  // 1. set provider
-  const provider = 'cosmos' // only cosmos is supported for now
-
-  // 2. instantiate tvm if ow credentials
-  let tvm
-  if (!config.cosmos) { // credentials.ow can be empty if env vars are set
-    const tvmArgs = { ow: config.ow, ...config.tvm }
-    tvm = await TvmClient.init(tvmArgs)
-  }
-
-  // 3. return state store based on provider
-  switch (provider) {
-    case 'cosmos':
-      return CosmosStateStore.init(config.cosmos || (await tvm.getAzureCosmosCredentials()))
-    // default:
-    //   throw new StateStoreError(`provider '${provider}' is not supported.`, StateStoreError.codes.BadArgument)
-  }
-}
-
-module.exports = { init }
+module.exports = require('./lib/init')
---
+++ test/init.test.js
@@ -0,0 +1,87 @@
+const stateStoreSDK = require('../index')
+
+const { CosmosStateStore } = require('../lib/impl/CosmosStateStore')
+jest.mock('../lib/impl/CosmosStateStore.js')
+
+const TvmClient = require('@adobe/adobeio-cna-tvm-client')
+jest.mock('@adobe/adobeio-cna-tvm-client')
+
+beforeEach(async () => {
+  expect.hasAssertions()
+  jest.restoreAllMocks()
+})
+
+describe('init', () => {
+  /* Common setup for init tests */
+  beforeEach(async () => {
+    CosmosStateStore.mockRestore()
+    CosmosStateStore.init = jest.fn()
+  })
+
+  describe('when user db credentials', () => {
+    const fakeCosmosConfig = {
+      fake: 'cosmosconfig'
+    }
+    test('with cosmos config', async () => {
+      await stateStoreSDK.init({ cosmos: fakeCosmosConfig })
+      expect(CosmosStateStore.init).toHaveBeenCalledTimes(1)
+      expect(CosmosStateStore.init).toHaveBeenCalledWith(fakeCosmosConfig)
+      expect(TvmClient.init).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe('with openwhisk credentials', () => {
+    const fakeTVMResponse = {
+      fakeTVMResponse: 'response'
+    }
+    const fakeOWCreds = {
+      auth: 'fake',
+      namespace: 'fake'
+    }
+    const fakeTVMOptions = {
+      some: 'options'
+    }
+    const cosmosTVMMock = jest.fn()
+    beforeEach(async () => {
+      TvmClient.mockReset()
+      TvmClient.init.mockReset()
+      cosmosTVMMock.mockReset()
+      TvmClient.init.mockResolvedValue({
+        getAzureCosmosCredentials: cosmosTVMMock
+      })
+    })
+    test('when tvm options', async () => {
+      cosmosTVMMock.mockResolvedValue(fakeTVMResponse)
+      await stateStoreSDK.init({ ow: fakeOWCreds, tvm: fakeTVMOptions })
+      expect(TvmClient.init).toHaveBeenCalledTimes(1)
+      expect(TvmClient.init).toHaveBeenCalledWith({ ow: fakeOWCreds, ...fakeTVMOptions })
+      expect(CosmosStateStore.init).toHaveBeenCalledTimes(1)
+      expect(CosmosStateStore.init).toHaveBeenCalledWith(fakeTVMResponse)
+    })
+    test('when empty config to be able to pass OW creds as env variables', async () => {
+      cosmosTVMMock.mockResolvedValue(fakeTVMResponse)
+      await stateStoreSDK.init()
+      expect(TvmClient.init).toHaveBeenCalledTimes(1)
+      expect(TvmClient.init).toHaveBeenCalledWith({ ow: undefined })
+      expect(CosmosStateStore.init).toHaveBeenCalledTimes(1)
+      expect(CosmosStateStore.init).toHaveBeenCalledWith(fakeTVMResponse)
+    })
+    test('when tvm rejects with a 401 (throws wrapped error)', async () => {
+      cosmosTVMMock.mockRejectedValue({ status: 401 })
+      await expect(stateStoreSDK.init.bind(stateStoreSDK, { ow: fakeOWCreds })).toThrowForbidden()
+    })
+    test('when tvm rejects with a 403 (throws wrapped error)', async () => {
+      cosmosTVMMock.mockRejectedValue({ status: 403 })
+      await expect(stateStoreSDK.init.bind(stateStoreSDK, { ow: fakeOWCreds })).toThrowForbidden()
+    })
+    test('when tvm rejects with another status code (throws tvm error)', async () => {
+      const tvmError = new Error({ status: 500 })
+      cosmosTVMMock.mockRejectedValue(tvmError)
+      try {
+        await stateStoreSDK.init({ ow: fakeOWCreds })
+      } catch (e) {
+        expect(e).toBe(tvmError)
+      }
+    })
+  })
+})
