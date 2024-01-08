12c13644a0df6f5563dfbb3e2c8e0a90687d3730
Shazron Abdullah <shaz@adobe.com>
2024-01-08 22:38:27 +0800

    fix: update docs for v3

--- types.d.ts
+++ types.d.ts
@@ -1,34 +1,52 @@
 /**
- * StateStore put options
+ * AdobeStateCredentials
+ * @property namespace - the state store namespace
+ * @property apikey - the state store api key
+ */
+export type AdobeStateCredentials = {
+    namespace: string;
+    apikey: string;
+};
+
+/**
+ * AdobeState put options
  * @property ttl - time-to-live for key-value pair in seconds, defaults to 24 hours (86400s). Set to < 0 for no expiry. A
  * value of 0 sets default.
  */
-export type StateStorePutOptions = {
+export type AdobeStatePutOptions = {
     ttl: number;
 };
 
 /**
- * StateStore get return object
+ * AdobeState get return object
  * @property expiration - ISO date string of expiration time for the key-value pair, if the ttl is infinite
  * expiration=null
  * @property value - the value set by put
  */
-export type StateStoreGetReturnValue = {
+export type AdobeStateGetReturnValue = {
     expiration: string | null;
     value: any;
 };
 
 /**
+ * Validates json according to a schema.
+ * @param schema - the AJV schema
+ * @param data - the json data to test
+ * @returns the result
+ */
+export function validate(schema: any, data: any): any;
+
+/**
  * Cloud State Management
  */
-export class StateStore {
+export class AdobeState {
     /**
      * Retrieves the state value for given key.
      * If the key doesn't exist returns undefined.
      * @param key - state key identifier
      * @returns get response holding value and additional info
      */
-    get(key: string): Promise<StateStoreGetReturnValue>;
+    get(key: string): Promise<AdobeStateGetReturnValue>;
     /**
      * Creates or updates a state key-value pair
      * @param key - state key identifier
@@ -36,7 +54,7 @@ export class StateStore {
      * @param [options = {}] - put options
      * @returns key
      */
-    put(key: string, value: any, options?: StateStorePutOptions): Promise<string>;
+    put(key: string, value: string, options?: AdobeStatePutOptions): Promise<string>;
     /**
      * Deletes a state key-value pair
      * @param key - state key identifier
@@ -44,22 +62,15 @@ export class StateStore {
      */
     delete(key: string): Promise<string>;
     /**
-     * @param key - state key identifier
-     * @returns get response holding value and additional info
+     * Deletes all key-values
+     * @returns true if deleted, false if not
      */
-    protected _get(key: string): Promise<StateStoreGetReturnValue>;
+    deleteAll(): Promise<boolean>;
     /**
-     * @param key - state key identifier
-     * @param value - state value
-     * @param options - state put options
-     * @returns key
+     * There exists key-values.
+     * @returns true if exists, false if not
      */
-    protected _put(key: string, value: any, options: any): Promise<string>;
-    /**
-     * @param key - state key identifier
-     * @returns key of deleted state or `null` if state does not exists
-     */
-    protected _delete(key: string): Promise<string>;
+    any(): Promise<boolean>;
 }
 
 /**
@@ -68,7 +79,7 @@ export class StateStore {
  * @property sdk - The SDK associated with the Error
  * @property sdkDetails - The SDK details associated with the Error
  */
-export type StateLibError = {
+export type AdobeStateLibError = {
     message: string;
     code: string;
     sdk: string;
@@ -76,25 +87,26 @@ export type StateLibError = {
 };
 
 /**
- * State lib custom errors.
+ * Adobe State lib custom errors.
  * `e.sdkDetails` provides additional context for each error (e.g. function parameter)
- * @property ERROR_BAD_ARGUMENT - this error is thrown when an argument is missing or has invalid type
+ * @property ERROR_BAD_ARGUMENT - this error is thrown when an argument is missing, has invalid type, or includes invalid characters.
  * @property ERROR_BAD_REQUEST - this error is thrown when an argument has an illegal value.
- * @property ERROR_NOT_IMPLEMENTED - this error is thrown when a method is not implemented or when calling
- * methods directly on the abstract class (StateStore).
  * @property ERROR_PAYLOAD_TOO_LARGE - this error is thrown when the state key, state value or underlying request payload size
  * exceeds the specified limitations.
  * @property ERROR_BAD_CREDENTIALS - this error is thrown when the supplied init credentials are invalid.
+ * @property ERROR_UNAUTHORIZED - this error is thrown when the credentials are unauthorized to access the resource
  * @property ERROR_INTERNAL - this error is thrown when an unknown error is thrown by the underlying
  * DB provider or TVM server for credential exchange. More details can be found in `e.sdkDetails._internal`.
+ * @property ERROR_REQUEST_RATE_TOO_HIGH - this error is thrown when the request rate for accessing state is too high.
  */
-export type StateLibErrors = {
-    ERROR_BAD_ARGUMENT: StateLibError;
-    ERROR_BAD_REQUEST: StateLibError;
-    ERROR_NOT_IMPLEMENTED: StateLibError;
-    ERROR_PAYLOAD_TOO_LARGE: StateLibError;
-    ERROR_BAD_CREDENTIALS: StateLibError;
-    ERROR_INTERNAL: StateLibError;
+export type AdobeStateLibErrors = {
+    ERROR_BAD_ARGUMENT: AdobeStateLibError;
+    ERROR_BAD_REQUEST: AdobeStateLibError;
+    ERROR_PAYLOAD_TOO_LARGE: AdobeStateLibError;
+    ERROR_BAD_CREDENTIALS: AdobeStateLibError;
+    ERROR_UNAUTHORIZED: AdobeStateLibError;
+    ERROR_INTERNAL: AdobeStateLibError;
+    ERROR_REQUEST_RATE_TOO_HIGH: AdobeStateLibError;
 };
 
 /**
@@ -108,44 +120,11 @@ export type OpenWhiskCredentials = {
 };
 
 /**
- * An object holding the Azure Cosmos resource credentials with permissions on a single partition and container
- * @property endpoint - cosmosdb resource endpoint
- * @property resourceToken - cosmosdb resource token restricted to the partitionKey
- * @property databaseId - id for cosmosdb database
- * @property containerId - id for cosmosdb container within database
- * @property partitionKey - key for cosmosdb partition within container authorized by resource token
- */
-export type AzureCosmosPartitionResourceCredentials = {
-    endpoint: string;
-    resourceToken: string;
-    databaseId: string;
-    containerId: string;
-    partitionKey: string;
-};
-
-/**
- * An object holding the Azure Cosmos account master key
- * @property endpoint - cosmosdb resource endpoint
- * @property masterKey - cosmosdb account masterKey
- * @property databaseId - id for cosmosdb database
- * @property containerId - id for cosmosdb container within database
- * @property partitionKey - key for cosmosdb partition where data will be stored
- */
-export type AzureCosmosMasterCredentials = {
-    endpoint: string;
-    masterKey: string;
-    databaseId: string;
-    containerId: string;
-    partitionKey: string;
-};
-
-/**
  * Initializes and returns the key-value-store SDK.
  *
  * To use the SDK you must either provide your
  * [OpenWhisk credentials]{@link OpenWhiskCredentials} in
  * `config.ow` or your own
- * [Azure Cosmos credentials]{@link AzureCosmosMasterCredentials} in `config.cosmos`.
  *
  * OpenWhisk credentials can also be read from environment variables `__OW_NAMESPACE` and `__OW_API_KEY`.
  * @param [config = {}] - used to init the sdk
@@ -153,19 +132,9 @@ export type AzureCosmosMasterCredentials = {
  * to use ootb credentials to access the state management service. OpenWhisk
  * namespace and auth can also be passed through environment variables:
  * `__OW_NAMESPACE` and `__OW_API_KEY`
- * @param [config.cosmos] - [Azure Cosmos resource credentials]{@link AzureCosmosPartitionResourceCredentials} or
- * [Azure Cosmos account credentials]{@link AzureCosmosMasterCredentials}
- * @param [config.tvm] - tvm configuration, applies only when passing OpenWhisk credentials
- * @param [config.tvm.apiUrl] - alternative tvm api url.
- * @param [config.tvm.cacheFile] - alternative tvm cache file, set to `false` to disable caching of temporary credentials.
- * @returns A StateStore instance
+ * @returns An AdobeState instance
  */
 export function init(config?: {
     ow?: OpenWhiskCredentials;
-    cosmos?: AzureCosmosMasterCredentials | AzureCosmosPartitionResourceCredentials;
-    tvm?: {
-        apiUrl?: string;
-        cacheFile?: string;
-    };
-}): Promise<StateStore>;
+}): Promise<AdobeState>;
 
