9afdc73737a0841f73e511a3497b6d87fcf5bfa7
Shazron Abdullah <36107+shazron@users.noreply.github.com>
2024-01-08 22:15:55 +0800

    feat: ACNA-2584 - add Adobe App Builder State Store (#134)
    * remove CosmosStateStore (users can use version 2.x for BYO creds support)
    * add support to override prod and stage endpoints (for local dev testing purposes)

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,13 @@ module.exports = {
   collectCoverageFrom: [
     'index.js',
     'lib/**/*.js'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      lines: 100,
+      statements: 100,
+      functions: 100
+    }
+  }
 }
