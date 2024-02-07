275f611bb51f9208f9c1b99292ddbb4b62f921b9
Shazron Abdullah <36107+shazron@users.noreply.github.com>
2024-02-07 22:26:37 +0800

    fix: endpoints will be internal/public according to Adobe Runtime context

--- test/utils.test.js
+++ test/utils.test.js
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-const { withHiddenFields } = require('../lib/utils')
+const { withHiddenFields, isInternalToAdobeRuntime } = require('../lib/utils')
 
 describe('withHiddenFields', () => {
   test('no params', () => {
@@ -39,5 +39,62 @@ describe('withHiddenFields', () => {
     }
 
     expect(withHiddenFields(src, ['cat'])).toEqual(target)
+  })
+})
+
+describe('isInternalToAdobeRuntime', () => {
+  const env = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...env }
+  })
+
+  afterEach(() => {
+    process.env = env
+  })
+
+  test('in runtime context', () => {
+    process.env.__OW_NAMESPACE = 'some-namespace'
+    process.env.__OW_API_HOST = 'some-server-dot-com'
+    process.env.__OW_ACTIVATION_ID = 'some-activation-id'
+
+    expect(isInternalToAdobeRuntime()).toBeTruthy()
+  })
+
+  test('not in runtime context', () => {
+    // make doubly sure the env vars are not there
+    delete process.env.__OW_NAMESPACE
+    delete process.env.__OW_API_HOST
+    delete process.env.__OW_ACTIVATION_ID
+
+    expect(isInternalToAdobeRuntime()).toBeFalsy()
+  })
+
+  test('in runtime context - endpoints should be internal', () => {
+    process.env.__OW_NAMESPACE = 'some-namespace'
+    process.env.__OW_API_HOST = 'some-server-dot-com'
+    process.env.__OW_ACTIVATION_ID = 'some-activation-id'
+
+    expect(isInternalToAdobeRuntime()).toBeTruthy()
+    jest.isolateModules(() => {
+      const constants = require('../lib/constants')
+      expect(constants.ADOBE_STATE_STORE_ENDPOINT.prod).toEqual(constants.ADOBE_STATE_STORE_ENDPOINT_PROD_INTERNAL)
+      expect(constants.ADOBE_STATE_STORE_ENDPOINT.stage).toEqual(constants.ADOBE_STATE_STORE_ENDPOINT_STAGE_INTERNAL)
+    })
+  })
+
+  test('not in runtime context - endpoints should be public', () => {
+    // make doubly sure the env vars are not there
+    delete process.env.__OW_NAMESPACE
+    delete process.env.__OW_API_HOST
+    delete process.env.__OW_ACTIVATION_ID
+
+    expect(isInternalToAdobeRuntime()).toBeFalsy()
+    jest.isolateModules(() => {
+      const constants = require('../lib/constants')
+      expect(constants.ADOBE_STATE_STORE_ENDPOINT.prod).toEqual(constants.ADOBE_STATE_STORE_ENDPOINT_PROD)
+      expect(constants.ADOBE_STATE_STORE_ENDPOINT.stage).toEqual(constants.ADOBE_STATE_STORE_ENDPOINT_STAGE)
+    })
   })
 })
