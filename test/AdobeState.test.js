72e7b45d5c5862e292db4864b1eff6c517a72f8e
Shazron Abdullah <shazron@gmail.com>
2024-03-21 22:36:57 +0800

    add AdobeStateLib:ERROR_INTERNAL returning response body contents

--- test/AdobeState.test.js
+++ test/AdobeState.test.js
@@ -55,14 +55,14 @@ const wrapInFetchResponse = (body, options = {}) => {
   }
 }
 
-const wrapInFetchError = (status) => {
+const wrapInFetchError = (status, body) => {
   return {
     ok: false,
     headers: {
       get: () => 'fake req id'
     },
-    json: async () => 'error',
-    text: async () => 'error',
+    json: async () => JSON.parse(body),
+    text: async () => body,
     status
   }
 }
@@ -253,9 +253,10 @@ describe('put', () => {
   test('coverage: unknown server error', async () => {
     const key = 'some-key'
     const value = 'some-value'
+    const responseBody = 'error: this is the response body'
 
-    mockExponentialBackoff.mockResolvedValue(wrapInFetchError(500))
-    await expect(store.put(key, value)).rejects.toThrow('[AdobeStateLib:ERROR_INTERNAL] unexpected response from provider with status: 500')
+    mockExponentialBackoff.mockResolvedValue(wrapInFetchError(500, responseBody))
+    await expect(store.put(key, value)).rejects.toThrow(`[AdobeStateLib:ERROR_INTERNAL] unexpected response from provider with status: 500 body: ${responseBody}`)
   })
 
   test('coverage: unknown error (fetch network failure)', async () => {
